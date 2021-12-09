b1783c03a722ceff67ee248ff3acec04864be056
Yagnesh Shukla <yag.shukla@gmail.com>
2021-12-09 09:59:10 +0000

    release for review

--- client/assets/script.js
+++ client/assets/script.js
@@ -5,25 +5,24 @@ const button2 = document.querySelector('ListSingleBtn');
 
 
 // event listener for button 1
-button1.addEventListener('click', function()
-{ 
+button1.addEventListener('click', e => getSearch(e))
+function getSearch(e){
   e.preventDefault()
   fetch('http://localhost:3000/search/all')
     .then(resp => resp.text)
     .then(text=>{document.getElementById('result').innerHTML= text})
 
-}
-); 
+};
 
-let form = document.querySelector('form')
- form.addEventListener("click", e => getRandom(e))
- function getRandom(e){
-    e.preventDefault()
-   fetch("http://localhost:3000/quoteRand")
-   .then(resp => resp.text())
-   .then(text => { document.getElementById("text").innerHTML=text
-     })
- }
+// let form = document.querySelector('form')
+//  form.addEventListener("click", e => getRandom(e))
+//  function getRandom(e){
+//     e.preventDefault()
+//    fetch("http://localhost:3000/quoteRand")
+//    .then(resp => resp.text())
+//    .then(text => { document.getElementById("text").innerHTML=text
+//      })
+//  }
 // event listener for button 2
 
 button2.addEventListener('click', function()
